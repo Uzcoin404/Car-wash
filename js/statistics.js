a35a4dae7689220b534613b8a888cbdcec7dad6d
Uzcoin404 <uzcointg@gmail.com>
2022-04-04 12:11:15 +0500

    some changes

---
+++ js/statistics.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('.main');
+const filterBtn = document.querySelector('.filter_btn');
+const filterBlog = document.querySelector('.filter_blog');
+
+filterBtn.addEventListener('click', function(){
+    filterBlog.classList.toggle('active');
+    main.style.overflow = 'hidden';
+});
